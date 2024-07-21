e7146d8d8355b669c250be0710b5051a807d8091
Dhereck1 <j.fuentealba.ross@gmail.com>
2024-07-21 17:34:55 -0400

    added react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
 
-import robotsTxt from "astro-robots-txt"
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  integrations: [tailwind(), robotsTxt(), react()],
+  site: 'https://juanfuentealba.vercel.app',
+  i18n: {
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+});
